80427d09ed64ac30e970a0ee89334a77d242b018
purnimagupta27 <purnimagupta2709@gmail.com>
2026-08-11 01:02:36 +0530

    made changes to post validation

--- src/dto/post.dto.js
+++ src/dto/post.dto.js
@@ -2,9 +2,12 @@ import { z } from 'zod'
 
 const createPostValidateSchema = z.object({
     caption: z.string().trim().max(500, "Caption can contain at most 500 characters").optional(),
-    isPrivate: z.coerce.boolean().default(false)
+    isPrivate: z.preprocess((val) => {
+        if (typeof val === "string") return val.toLowerCase() === "true";
+        return Boolean(val);
+    }, z.boolean().default(false))
 })
 
 export {
     createPostValidateSchema
-}
+}
